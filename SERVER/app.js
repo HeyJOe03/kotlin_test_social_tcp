2f8574843c44c135fa8e4935b87948e421b5510f
HeyJOe03 <jocarmi03@gmail.com>
2021-06-14 11:08:20 +0200

    server side for a group chat

--- SERVER/app.js
+++ SERVER/app.js
@@ -23,7 +23,7 @@ io.on('connection',(socket) => {
 
     //io.to(socket.id).emit('message',{sender:"server",to:socket.id,message:"hello client"})
 
-    
+
     socket.on('join_chat', (username) => {
         element_me = new User(username,socket.id)
         users.add(element_me)
@@ -36,19 +36,23 @@ io.on('connection',(socket) => {
     socket.on('message', ({sender,to,message}) => {
         console.log({sender,to,message})
 
-        toSocket = users.find(e => {
-            return e.username == to
-        })[0].address
+        // toSocket = users.find(e => {
+        //     return e.username == to
+        // })[0].address
 
-        console.log(toSocket)
+        // console.log(toSocket)
 
-        io.to(toSocket).emit('message',{sender,to,message})
+        // io.to(toSocket).emit('message',{sender,to,message})
+
+        io.broadcast.emit({sender,to,message})
     })
 
     socket.on('disconnect', () => {
         console.log(socket.id + " left the chat")
 
         //FIXME: to check
+
+        io.emit({sender:"server",to:"all",message:`user: ${socket.id} left the chat`})
 
         users.removeElement(element_me)
     })
